c80e3469e0a4c7b9e6970ad49e738646fad630d5
Let64 <leticia.o.santos@edu.senai.br>
2026-09-17 16:27:24 -0300

    find

---
+++ 03-find.js
@@ -0,0 +1,30 @@
+// MÉTODO: find()
+//Função:Percorrer o array e retorna o PRIMEIRO ELEMENTO que satisfaz a condição. a condição.
+//Se nenhum for encontrado, retorna undefined.
+
+console.log("======EXEMPLOS COM find() ======\n");
+
+//==============================
+//EXEMPLO 1 (BÁSICO): Encontrar o primeiro número par
+//==============================
+
+console.log("-----------Exemplo 1: Primeira nota aprovada (Básico) -----------");
+
+const notas = [4.5, 5.8, 7.5, 9.0, 6.0];
+
+const primeiraNotaAprovada = notas
+
+
+
+
+
+
+
+
+
+
+
+//---------------------------------------------------
+//Exemplo 2 (INTERMEDIÁRIO): Buscar usuario por nome 
+//----------------------------------------------------
+
